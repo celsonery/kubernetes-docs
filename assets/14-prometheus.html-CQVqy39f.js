4b6a977f38946c1d00f88821791868a02dde3490
celsonery <celso.nery@gmail.com>
2025-05-26 22:58:39 +0000

    Deploying to gh-pages from @ celsonery/kubernetes-docs@c683d336cbae671bea6c73b24916f09d4f14e6a5 🚀

---
+++ assets/14-prometheus.html-CQVqy39f.js
@@ -0,0 +1,21 @@
+import{_ as e,c as s,o as a,d as n}from"./app-BMZpApWF.js";const i={},l=n(`<h1 id="prometheus" tabindex="-1"><a class="header-anchor" href="#prometheus"><span>Prometheus</span></a></h1><p>1 - Instale o Helm</p><p>2 - Instale ou confirme se o Metrics-server está instalado</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh" data-title="sh"><pre><code><span class="line">kubectl <span class="token function">top</span> nodes</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div></div></div><p>3 - Instale o prometheus</p><p>https://artifacthub.io/</p><p>Instalar o Chart</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh" data-title="sh"><pre><code><span class="line"><span class="token variable">$helm</span> repo <span class="token function">add</span> prometheus-community https://prometheus-community.github.io/helm-charts</span>
+<span class="line"></span>
+<span class="line">$ helm repo update</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>$ helm repo list</p><p>$ helm show values prometheus-community/prometheus &gt; prometheus-values.yaml</p><p>$ helm install prometheus prometheus-community/prometheus --values prometheus-values.yaml</p><p>testar</p><p>https://adm.bagarote.com.br:30001</p><p>https://adm.bagarote.com.br:30001/metrics</p><p>Habilitar a aplicacao</p><p>adicionar no manifest da aplicacao</p><p>spec: template: metadata: annotacions: prometheus.io/scrape: &quot;true&quot; prometheus.io/path: /metrics prometheus.io/port: &quot;8080&quot; labels: app: api ...</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line"></span>
+<span class="line">#### Kube-state-metrics</span>
+<span class="line"></span>
+<span class="line">1 - service-account.yaml</span>
+<span class="line">2 - cluster-role.yaml</span>
+<span class="line">3 - cluster-role-binding.yaml</span>
+<span class="line">4 - deployment.yaml</span>
+<span class="line">5 - service.yaml</span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line">## Instalação via yaml</span>
+<span class="line">1 - namespace</span>
+<span class="line">2 - clusterRole</span>
+<span class="line">3 - config-map</span>
+<span class="line">4 - prometheus-deployment</span>
+<span class="line">5 - prometheus-service</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,18),t=[l];function p(r,c){return a(),s("div",null,t)}const o=e(i,[["render",p],["__file","14-prometheus.html.vue"]]),d=JSON.parse('{"path":"/br/kubernetes/14-prometheus.html","title":"Prometheus","lang":"Português","frontmatter":{},"headers":[],"git":{"updatedTime":1748002949000,"contributors":[{"name":"Celso Nery","email":"celso.nery@gmail.com","commits":1}]},"filePathRelative":"br/kubernetes/14-prometheus.md"}');export{o as comp,d as data};
---
+++ assets/16-ingress-controller.html-CTw0y6qq.js
@@ -0,0 +1,39 @@
+import{_ as s,c as n,o as a,d as e}from"./app-BMZpApWF.js";const p={},l=e(`<h1 id="ingress-controller" tabindex="-1"><a class="header-anchor" href="#ingress-controller"><span>Ingress Controller</span></a></h1><ul><li><p>Site https://kubernetes.github.io/ingress-nginx/</p></li><li><p>Instalação via helm helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace</p></li><li><p>Criar ingress.yaml</p></li></ul><div class="language-yaml line-numbers-mode" data-highlighter="prismjs" data-ext="yml" data-title="yml"><pre><code><span class="line"><span class="token key atrule">apiVersion</span><span class="token punctuation">:</span> networking.k8s.io/v1</span>
+<span class="line"><span class="token key atrule">kind</span><span class="token punctuation">:</span> Ingress</span>
+<span class="line"><span class="token key atrule">metadata</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token key atrule">name</span><span class="token punctuation">:</span> ingress<span class="token punctuation">-</span>hosts</span>
+<span class="line">  <span class="token key atrule">annotations</span><span class="token punctuation">:</span></span>
+<span class="line">    <span class="token key atrule">kubernetes.io/ingress.class</span><span class="token punctuation">:</span> <span class="token string">&quot;nginx&quot;</span></span>
+<span class="line"><span class="token key atrule">spec</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token key atrule">rules</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">host</span><span class="token punctuation">:</span> api.bagarote.com.br</span>
+<span class="line">    <span class="token key atrule">http</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token key atrule">paths</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> <span class="token key atrule">pathType</span><span class="token punctuation">:</span> <span class="token string">&quot;Prefix&quot;</span></span>
+<span class="line">        <span class="token key atrule">path</span><span class="token punctuation">:</span> <span class="token string">&quot;/&quot;</span></span>
+<span class="line">        <span class="token key atrule">backend</span><span class="token punctuation">:</span></span>
+<span class="line">          <span class="token key atrule">service</span><span class="token punctuation">:</span></span>
+<span class="line">            <span class="token key atrule">name</span><span class="token punctuation">:</span> api</span>
+<span class="line">            <span class="token key atrule">port</span><span class="token punctuation">:</span></span>
+<span class="line">              <span class="token key atrule">number</span><span class="token punctuation">:</span> <span class="token number">8082</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">host</span><span class="token punctuation">:</span> adm.bagarote.com.br</span>
+<span class="line">    <span class="token key atrule">http</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token key atrule">paths</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> <span class="token key atrule">pathType</span><span class="token punctuation">:</span> <span class="token string">&quot;Prefix&quot;</span></span>
+<span class="line">        <span class="token key atrule">path</span><span class="token punctuation">:</span> <span class="token string">&quot;/&quot;</span></span>
+<span class="line">        <span class="token key atrule">backend</span><span class="token punctuation">:</span></span>
+<span class="line">          <span class="token key atrule">service</span><span class="token punctuation">:</span></span>
+<span class="line">            <span class="token key atrule">name</span><span class="token punctuation">:</span> adm</span>
+<span class="line">            <span class="token key atrule">port</span><span class="token punctuation">:</span></span>
+<span class="line">              <span class="token key atrule">number</span><span class="token punctuation">:</span> <span class="token number">80</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">host</span><span class="token punctuation">:</span> site.bagarote.com.br</span>
+<span class="line">    <span class="token key atrule">http</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token key atrule">paths</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> <span class="token key atrule">pathType</span><span class="token punctuation">:</span> <span class="token string">&quot;Prefix&quot;</span></span>
+<span class="line">        <span class="token key atrule">path</span><span class="token punctuation">:</span> <span class="token string">&quot;/&quot;</span></span>
+<span class="line">        <span class="token key atrule">backend</span><span class="token punctuation">:</span></span>
+<span class="line">          <span class="token key atrule">service</span><span class="token punctuation">:</span></span>
+<span class="line">            <span class="token key atrule">name</span><span class="token punctuation">:</span> site</span>
+<span class="line">            <span class="token key atrule">port</span><span class="token punctuation">:</span></span>
+<span class="line">              <span class="token key atrule">number</span><span class="token punctuation">:</span> <span class="token number">80</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),t=[l];function i(c,o){return a(),n("div",null,t)}const r=s(p,[["render",i],["__file","16-ingress-controller.html.vue"]]),k=JSON.parse('{"path":"/br/kubernetes/16-ingress-controller.html","title":"Ingress Controller","lang":"Português","frontmatter":{},"headers":[],"git":{"updatedTime":1748002949000,"contributors":[{"name":"Celso Nery","email":"celso.nery@gmail.com","commits":1}]},"filePathRelative":"br/kubernetes/16-ingress-controller.md"}');export{r as comp,k as data};
